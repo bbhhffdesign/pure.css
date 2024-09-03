62bebeccd4b66447c0620739e100c179d573698e
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-09-03 00:04:03 -0300

    animacion mindMap terminada

--- js/mindMapAni.js
+++ js/mindMapAni.js
@@ -72,6 +72,7 @@ let mmTabletSmall = gsap.matchMedia();
 let mmDesktopSmall = gsap.matchMedia();
 let mmDesktopMed = gsap.matchMedia();
 let mmDesktopLarge = gsap.matchMedia();
+let mmDesktopXLarge = gsap.matchMedia();
 
 let funcTimelines = function (timelines) {
   timelines
@@ -139,8 +140,6 @@ let funcTimelines = function (timelines) {
 let funcTimelinesDesktopSmall = function (timelines) {
   timelines
 
-
-    // .to(scrollDownIconDesktop,{y:"-60vh"})
    
     .from(dueño, { opacity: 0 })
     .to(flecha1, { strokeDashoffset: 0 })
@@ -338,6 +337,72 @@ let funcTimelinesDesktopLarge = function (timelines) {
     .from(logoYamatec, { opacity: 0 }, "<+20%")
 
 };
+let funcTimelinesDesktopXLarge = function (timelines) {
+  timelines
+    .from(dueño, { opacity: 0 })
+    .to(flecha1, { strokeDashoffset: 0 })
+    .from(no1, { opacity: 0 }, "<50%")
+    .from(agenda, { opacity: 0 })
+    .to(flecha2, { strokeDashoffset: 0 })
+    .from(si1, { opacity: 0 }, "<50%")
+    .from(posicionarMarca, { opacity: 0 })
+    .to(flecha3, { strokeDashoffset: 0 })
+    .from(si2, { opacity: 0 }, "<50%")
+    .from(branding, { opacity: 0 })
+    .to(flecha4, { strokeDashoffset: 0 })
+    .from(no2, { opacity: 0 }, "<50%")
+    .from(excelente, { opacity: 0 })
+    .from(
+      iconoLike,
+      {
+        opacity: 0,
+        scale: 0.5,
+        transformOrigin: "50% 50%",
+        ease: "bounce.out",
+        duration: 0.8,
+      },
+      "<"
+    )
+    .from(aumentarVentas, { opacity: 0 })
+    .to(flecha5, { strokeDashoffset: 0 })
+    .to(mindMap,{y:"-10%"},"<") 
+    .from(si3, { opacity: 0 }, "<50%")
+    .from(estrategia, { opacity: 0 })
+    .to(flecha6, { strokeDashoffset: 0 })
+    .to(flecha7, { strokeDashoffset: 0 }, "<")
+    .from(agendaBig, { opacity: 0 })
+    .from(agendaBigBg, { opacity: 0 }, "<")
+    .to(flecha8, { strokeDashoffset: 0 })
+    .to(mindMap,{y:"-10%"},"<") 
+    .from(no3, { opacity: 0 }, "<10%")
+    .to(mindMap,{y:"-10%"},"<")
+    .from(camino, { opacity: 0 })
+    .from(
+      iconoTick,
+      {
+        opacity: 0,
+        scale: 0.5,
+        transformOrigin: "50% 50%",
+        ease: "bounce.out",
+        duration: 0.8,
+      },
+      "<"
+    )
+    .to(mindMap,{y:"-42%"})
+    .to(flechaF1,{strokeDashoffset: 0},"<+4%")
+    .to(flechaF2,{strokeDashoffset: 0},"<+8%")
+    .to(flechaF3,{strokeDashoffset: 0},"<+12%")
+    .to(flechaF4,{strokeDashoffset: 0},"<+16%")
+    .to(flechaF5,{strokeDashoffset: 0},"<+20%")
+    .to(flechaF6,{strokeDashoffset: 0},"<+24%")
+    .from(logoGallmarquez, { opacity: 0 }, "<")
+    .from(logoUdana, { opacity: 0 }, "<+4%")
+    .from(logoX, { opacity: 0 }, "<+8%")
+    .from(logoBelen, { opacity: 0 }, "<+12%")
+    .from(logoSound, { opacity: 0 }, "<+16%")
+    .from(logoYamatec, { opacity: 0 }, "<+20%")
+
+};
 
 mmMobile.add("(max-width: 599px)", () => {
   let tlMobile = gsap.timeline({
@@ -370,17 +435,17 @@ mmTabletSmall.add("(min-width:600px) and (max-width: 768px)", () => {
 });
 
 mmDesktopSmall.add("(min-width:769px) and (max-width: 1279px)", () => {
-  let tlMindmapDesktop = gsap.timeline({
+  let tlMindmapDesktopSmall = gsap.timeline({
     scrollTrigger: {
       trigger: heroContainer,
       scrub: 1,
-      // markers: true,
+      markers: true,
       pin: true,
-      start: "top 8%",
+      start: "8% 8%",
       end: "600% bottom",
     },
   });
-  funcTimelinesDesktop(tlMindmapDesktopSmall)
+  funcTimelinesDesktopSmall(tlMindmapDesktopSmall)
 });
 
 mmDesktopMed.add("(min-width: 1280px) and (max-width: 1439px)", () => {
@@ -388,24 +453,37 @@ mmDesktopMed.add("(min-width: 1280px) and (max-width: 1439px)", () => {
     scrollTrigger: {
       trigger: heroContainer,
       scrub: 1,
-      // markers: true,
+      markers: true,
       pin: true,
-      start: "top 8%",
+      start: "10% 10%",
       end: "1000% bottom",
     },
   });
   funcTimelinesDesktopMed(tlMindmapDesktopMed)
 });
-mmDesktopLarge.add("(min-width: 1440px)", () => {
+mmDesktopLarge.add("(min-width: 1440px) and (max-width: 1919px)", () => {
   let tlMindmapDesktopLarge = gsap.timeline({
     scrollTrigger: {
       trigger: heroContainer,
       scrub: 1,
       // markers: true,
       pin: true,
-      start: "top 8%",
-      end: "1000% bottom",
+      start: "10% 10%",
+      end: "600% bottom",
     },
   });
-  funcTimelinesDesktopLarge(tlMindmapDesktopLarge)
+  funcTimelinesDesktopXLarge(tlMindmapDesktopLarge)
+});
+mmDesktopXLarge.add("(min-width: 1920px)", () => {
+  let tlMindmapDesktopXLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: heroContainer,
+      scrub: 1,
+      markers: true,
+      pin: true,
+      start: "10% 10%",
+      end: "600% bottom",
+    },
+  });
+  funcTimelinesDesktopXLarge(tlMindmapDesktopXLarge)
 });
