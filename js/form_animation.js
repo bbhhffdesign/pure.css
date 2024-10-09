2b5f8376791611f816e5217957cbabb198b36c02
bbhhffdesign <bbhhff.design@gmail.com>
2024-10-09 19:59:41 -0300

    a

--- js/form_animation.js
+++ js/form_animation.js
@@ -2,4 +2,25 @@
 
 const input1Box = document.getElementById("input1Box")
 const nombre = document.getElementById("input1-data")
-const input1siguiente = document.getElementById("input1-")
+const input1siguiente = document.getElementById("input1-siguiente")
+
+//input1
+
+const input2Box = document.getElementById("input2Box")
+const email = document.getElementById("input2-data")
+const input2volver = document.getElementById("input2-volver")
+const input2siguiente = document.getElementById("input2-siguiente")
+
+//input1
+
+const input3Box = document.getElementById("input3Box")
+const marca = document.getElementById("input3-data")
+const input3volver = document.getElementById("input3-volver")
+const input3siguiente = document.getElementById("input3-siguiente")
+
+//input1
+
+const input4Box = document.getElementById("input4Box")
+const redSocial = document.getElementById("input4-data")
+const input4volver = document.getElementById("input4-volver")
+const input4siguiente = document.getElementById("input4-siguiente")
