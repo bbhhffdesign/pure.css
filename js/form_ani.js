de0f3767bda8b9cb44be49180012725d77d18792
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-10-11 23:48:59 -0300

    a

--- js/form_ani.js
+++ js/form_ani.js
@@ -4,6 +4,7 @@ const error_return = document.getElementById("input_error_return");
 const error_text = document.getElementById("input_error_text");
 
 //form containers
+const inputA = document.getElementById("input_a"); 
 const inputA_container = document.getElementById("input_a_container");
 const inputA_next = document.getElementById("input_a_next");
 const inputA_back = document.getElementById("input_a_back");
@@ -82,8 +83,8 @@ tl_inputD_error
 const tl_inputE_next = gsap.timeline({ paused: true });
 tl_inputE_next
   .to(inputE_container, { x: "-600%" })
-  .to(inputF_container,{x: "-600%"})
-  .to(form_container,{y: "100%", delay: 3, duration:2})
+  .to(inputF_container, { x: "-600%" })
+  .to(form_container, { y: "100%", delay: 3, duration: 2 });
 //timelines back
 const tl_inputB_back = gsap.timeline({ paused: true });
 tl_inputB_back
@@ -111,10 +112,12 @@ inputA_next.addEventListener("click", () => {
 
   if (name.length < 10) {
     error_text.innerText = "Tu nombre debe contener mas de 10 caracteres.";
-    tl_inputA_error.restart();
+    // tl_inputA_error.restart();
+    gsap.to(inputA_container,{x:"+=7", yoyo:true, repeat:3 ,duration: .03});
+    // inputA.placeholder = "ERROR";
 
     error_return.addEventListener("click", () => {
-      tl_inputA_error.reverse();
+      // tl_inputA_error.reverse();
     });
   } else {
     display_name.innerText = name;
@@ -227,7 +230,7 @@ tl_gemstoneBg
   .to(gall, { strokeDashoffset: 0, duration: 6 })
   .to(gall, {
     strokeDashoffset: gall_path,
-    duration: 3,
+    duration: 2,
     delay: 1,
     opacity: 0.5,
   })
@@ -235,3 +238,27 @@ tl_gemstoneBg
   .to(gemstoneSup, { strokeDashoffset: 0, duration: 10 }, "<")
   .to(gemstoneInf, { strokeDashoffset: gemstoneInf_path, duration: 5 })
   .to(gemstoneSup, { strokeDashoffset: gemstoneSup_path, duration: 5 }, "<");
+
+
+
+  // gsap.to(inputA_container,{x:"+=20", yoyo:true, repeat:1,duration: .05})
+
+  
+
+// gsap.to(bordeInt, { strokeDashoffset: 0, duration: 0 });
+// gsap.to(bordeExt, { strokeDashoffset: 0, duration: 0 }, "<");
+// tl_gemstoneBg
+
+//   .to(gall, { strokeDashoffset: 0, duration: 1,  delay:.5})
+
+  // .to(gall, { strokeDashoffset: 0, duration: 2 , delay:1})
+  // .to(gall, {
+  //   strokeDashoffset: gall_path,
+  //   duration: 2,
+  //   delay: 1,
+  //   opacity: 0.5,
+  // })
+  // .to(gemstoneInf, { strokeDashoffset: 0, duration: 6 })
+  // .to(gemstoneSup, { strokeDashoffset: 0, duration: 6 , ease: "power4.out"}, "<");
+// .to(gemstoneInf, { strokeDashoffset: gemstoneInf_path, duration: 5 })
+// .to(gemstoneSup, { strokeDashoffset: gemstoneSup_path, duration: 5 }, "<");
