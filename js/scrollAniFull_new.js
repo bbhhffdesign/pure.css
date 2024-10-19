6f67221eb128b7ab215e84e90d3fa918be0b7ae9
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-10-19 01:01:52 -0300

    a

--- js/scrollAniFull_new.js
+++ js/scrollAniFull_new.js
@@ -67,8 +67,47 @@ const orbita_texts = [
     document.getElementById("orbita-text5"),
     document.getElementById("orbita-text6")
 ]
-//scale,duration
-const paramAnim =[0.9, 0.6, "90%"];
+//
+// scale,duration
+const paramAnim =[0.9, 1, "90%"];
+
+gsap.timeline({
+  scrollTrigger:{
+    trigger: ".mindmap-title-container",
+    start: "top 70%",
+    end: "top 70%",
+    // markers: true,
+    // scrub: true
+  }
+})
+.from(".mindmap-title-container", {
+  scale: paramAnim[0],
+  transformOrigin: "50% 50%",
+  opacity: 0,
+  y: 150,
+  stagger: 0.1, // Añadir un pequeño retardo entre animaciones
+  ease: "power4.out",
+  duration: paramAnim[1],
+});
+
+gsap.timeline({
+  scrollTrigger:{
+    trigger: ".gemstone-text",
+    start: "bottom 90%",
+    end: "bottom 90%",
+    markers: true,
+    // scrub: true
+  }
+})
+.from(".gem-txt", {
+  scale: paramAnim[0],
+  transformOrigin: "50% 50%",
+  opacity: 0,
+  y: 150,
+  stagger: 0.1, // Añadir un pequeño retardo entre animaciones
+  ease: "power4.out",
+  duration: paramAnim[1],
+});
 
 const tl_navbar = gsap.timeline({
   scrollTrigger: {
@@ -83,55 +122,55 @@ tl_navbar
   .to(navBar, { y: "-100%" })
   .from(themeSwitch2, { opacity: 0, zIndex: "auto" }, "<");
 
-for (let i = 0; i < mindmapTexts.length; i++) {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: mindmapTexts[i],
-      start: `-30% ${paramAnim[2]}`,
-      end: "300% 100%",
-      // markers: true
-      // scrub: 1,
-    },
-  });
-  tl.from(mindmapTexts[i], {
-    scale: paramAnim[0],
-    transformOrigin: "50% 50%",
-    opacity: 0,
-    y: 150,
-    ease: "power4.out",
-    duration: paramAnim[1],
-  });
-}
+// for (let i = 0; i < mindmapTexts.length; i++) {
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: mindmapTexts[i],
+//       start: `-30% ${paramAnim[2]}`,
+//       end: "300% 100%",
+//       // markers: true
+//       // scrub: 1,
+//     },
+//   });
+//   tl.from(mindmapTexts[i], {
+//     scale: paramAnim[0],
+//     transformOrigin: "50% 50%",
+//     opacity: 0,
+//     y: 150,
+//     ease: "power4.out",
+//     duration: paramAnim[1],
+//   });
+// }
 
-for (let i = 0; i < servicesBoxes.length; i++) {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      //   markers: true,
-      trigger: servicesBoxes[i],
-      start: `top ${paramAnim[2]}`,
-      end: "500% 90%",
-      // scrub: 1,
-    },
-  });
-  tl.from(servicesBoxContent[i], {
-    scale: paramAnim[0],
-    transformOrigin: "50% 50%",
-    opacity: 0,
-    y: 150,
-    ease: "power4.out",
-    duration: paramAnim[1],
-  })
-    // .from(servicesBoxLogos[i], {
-    //   duration: 3,
-    //   fill: "#c6c6c6",
-    //   ease: "power4.out",
-    // })
-    // .to(servicesBoxLogos[i], {
-    //   duration: 1,
-    //   fill: "var(--colorBg)",
-    //   ease: "power4.out",
-    // });
-}
+// for (let i = 0; i < servicesBoxes.length; i++) {
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       //   markers: true,
+//       trigger: servicesBoxes[i],
+//       start: `top ${paramAnim[2]}`,
+//       end: "500% 90%",
+//       // scrub: 1,
+//     },
+//   });
+//   tl.from(servicesBoxContent[i], {
+//     scale: paramAnim[0],
+//     transformOrigin: "50% 50%",
+//     opacity: 0,
+//     y: 150,
+//     ease: "power4.out",
+//     duration: paramAnim[1],
+//   })
+//     // .from(servicesBoxLogos[i], {
+//     //   duration: 3,
+//     //   fill: "#c6c6c6",
+//     //   ease: "power4.out",
+//     // })
+//     // .to(servicesBoxLogos[i], {
+//     //   duration: 1,
+//     //   fill: "var(--colorBg)",
+//     //   ease: "power4.out",
+//     // });
+// }
 
 const tl_gemstoneText_1 = gsap.timeline({
   scrollTrigger: {
@@ -187,99 +226,99 @@ for (let i = 0; i < gemBg.length; i++) {
   gemBg_anim_tl.to(gemBg[i], { strokeDashoffset: 0, duration: 5 }, "<");
 }
 
-for (let i = 0; i < gemstoneTexts.length; i++) {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: gemstoneTexts[i],
-      //   markers: true,
-      start: `-20% ${paramAnim[2]}`,
-      end: "500% 90%",
-      // scrub: true,
-    },
-  });
-  tl.from(gemstoneTexts[i], {
-    opacity: 0,
-    scale: paramAnim[0],
-    transformOrigin: "50% 50%",
-    y: 150,
-    duration: paramAnim[1],
-  });
-}
+// for (let i = 0; i < gemstoneTexts.length; i++) {
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: gemstoneTexts[i],
+//       //   markers: true,
+//       start: `-20% ${paramAnim[2]}`,
+//       end: "500% 90%",
+//       // scrub: true,
+//     },
+//   });
+//   tl.from(gemstoneTexts[i], {
+//     opacity: 0,
+//     scale: paramAnim[0],
+//     transformOrigin: "50% 50%",
+//     y: 150,
+//     duration: paramAnim[1],
+//   });
+// }
 
-for (let i = 0; i < pasos.length; i++) {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      // markers: true,
-      trigger: pasos[i],
-      start: `-50% ${paramAnim[2]}`,
-      end: "bottom 90%",
-      // scrub: true,
-    },
-  });
-  tl.from(pasos[i], {
-    opacity: 0,
-    scale: paramAnim[0],
-    transformOrigin: "50% 50%",
-    duration: paramAnim[1],
-    y: 150,
-  })
-    .from(pasos_number[i], {
-      opacity: 0,
-      scale: paramAnim[0],
-      transformOrigin: "50% 50%",
-      duration: paramAnim[1],
-      y: 150,
-    }, "<");
-}
-const tl_ctaGemstone = gsap.timeline({
-  scrollTrigger: {
-    trigger: ctaGemstone,
-    // markers:true,
-    start: `top ${paramAnim[2]}`,
-    end: "bottom 90%",
-  },
-});
-tl_ctaGemstone.from(ctaGemstone, { 
-  duration: paramAnim[1],
-  scale: paramAnim[0],
-  transformOrigin: "50% 50%",
-  opacity: 0,
-  y: 100 });
+// for (let i = 0; i < pasos.length; i++) {
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       // markers: true,
+//       trigger: pasos[i],
+//       start: `-50% ${paramAnim[2]}`,
+//       end: "bottom 90%",
+//       // scrub: true,
+//     },
+//   });
+//   tl.from(pasos[i], {
+//     opacity: 0,
+//     scale: paramAnim[0],
+//     transformOrigin: "50% 50%",
+//     duration: paramAnim[1],
+//     y: 150,
+//   })
+//     .from(pasos_number[i], {
+//       opacity: 0,
+//       scale: paramAnim[0],
+//       transformOrigin: "50% 50%",
+//       duration: paramAnim[1],
+//       y: 150,
+//     }, "<");
+// }
+// const tl_ctaGemstone = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ctaGemstone,
+//     // markers:true,
+//     start: `top ${paramAnim[2]}`,
+//     end: "bottom 90%",
+//   },
+// });
+// tl_ctaGemstone.from(ctaGemstone, { 
+//   duration: paramAnim[1],
+//   scale: paramAnim[0],
+//   transformOrigin: "50% 50%",
+//   opacity: 0,
+//   y: 100 });
 
 
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: cta_email,
-    // markers: true,
-    start: `top ${paramAnim[2]}`,
-    end: "200% 95%",
-    // scrub: true
-  }
-});
-tl
-  .from(cta_email, {
-    scale: paramAnim[0],
-    transformOrigin: "50% 50%",
-    duration: paramAnim[1],
-    opacity: 0,
-    y: 150,
-  })
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: cta_email,
+//     // markers: true,
+//     start: `top ${paramAnim[2]}`,
+//     end: "200% 95%",
+//     // scrub: true
+//   }
+// });
+// tl
+//   .from(cta_email, {
+//     scale: paramAnim[0],
+//     transformOrigin: "50% 50%",
+//     duration: paramAnim[1],
+//     opacity: 0,
+//     y: 150,
+//   })
 
-  for (let i = 0; i < orbita_texts.length; i++){
-    const tl = gsap.timeline({
-      scrollTrigger:{
-        trigger: orbita_texts[i],
-        start: `top ${paramAnim[2]}`,
-        end: "bottom 90%",
-        // markers: true
-      }
-    });
-    tl
-      .from(orbita_texts[i], {
-        scale: paramAnim[0],
-        transformOrigin: "50% 50%",
-        duration: paramAnim[1],
-        opacity: 0,
-        y: 150,
-      })
-  }
+//   for (let i = 0; i < orbita_texts.length; i++){
+//     const tl = gsap.timeline({
+//       scrollTrigger:{
+//         trigger: orbita_texts[i],
+//         start: `top ${paramAnim[2]}`,
+//         end: "bottom 90%",
+//         // markers: true
+//       }
+//     });
+//     tl
+//       .from(orbita_texts[i], {
+//         scale: paramAnim[0],
+//         transformOrigin: "50% 50%",
+//         duration: paramAnim[1],
+//         opacity: 0,
+//         y: 150,
+//       })
+//   }
