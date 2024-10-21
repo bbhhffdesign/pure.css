f74ac1f52e29432393181ec8467d7f936f43dfed
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-10-21 03:25:10 -0300

    a

--- js/aaah.js
+++ js/aaah.js
@@ -1,5 +1,3 @@
-const serviceBox1 = document.getElementById("services-box-1");
-
 const cajasServicio = [
     document.getElementById("services-box-1"),
     document.getElementById("services-box-2"),
@@ -15,10 +13,23 @@ for ( let i = 0; i < cajasServicio.length; i++){
     })
 }
 
-// serviceBox1.addEventListener("touchstart", ()=>{
-//     serviceBox1.classList.add("active");
-// })
-// serviceBox1.addEventListener("touchend", ()=>{
-//     serviceBox1.classList.remove("active");
-// })
+const cajaPasos = [
+    document.getElementById("paso-1-content"),
+    document.getElementById("paso-2-content"),
+    document.getElementById("paso-3-content"),
+]
+const pasosNum = [
+        document.getElementById("paso-number-1"),
+        document.getElementById("paso-number-2"),
+        document.getElementById("paso-number-3"),
+]
+
+for ( let i = 0; i < cajaPasos.length; i++){
+    cajaPasos[i].addEventListener("touchstart",() => {
+        pasosNum[i].classList.add("paso__active")   
+    });
+    cajaPasos[i].addEventListener("touchend",() => {
+        pasosNum[i].classList.remove("paso__active")   
+    });
+}
 
