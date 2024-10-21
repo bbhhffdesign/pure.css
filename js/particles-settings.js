bfe0db3535b7038c19747dd56f8af3a2af227cb2
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-10-20 23:38:30 -0300

    a

--- js/particles-settings.js
+++ js/particles-settings.js
@@ -3,65 +3,69 @@
 
   // Verificar el ancho del viewport
   const isDesktop = window.innerWidth > 768;
-  const particleQuantity = isDesktop ? 3 : 3; 
+  const particleQuantity = isDesktop ? 3 : 3;
 
   await tsParticles.load({
-      id: "tsparticles",
-      options: {
-          particles: {
-              number: {
-                  value: 0,
-              },
-              color: {
-                  value: "#0B4599",
-              },
-              links: {
-                  color: "#003399",
-              },
-              move: {
-                  enable: true,
-                  speed: 1,
-                  outModes: {
-                      default: "bounce", 
-                  },
-              },
-              size: {
-                  value: { min: 150, max: 200 },
-              },
-              opacity: {
-                  value: 1,
-              },
-          },
-          emitters: {
-              direction: "none",
-              position: {
-                  x: 50, 
-                  y: 50,
-              },
-              rate: {
-                  quantity: particleQuantity, 
-                  delay: 1,
-              },
-              autoPlay: true, 
-              life: {
-                  duration: 1,
-                  count: 1, 
-              },
-              particles: {
-                  move: {
-                      speed: 1.5,
-                      outModes: {
-                          default: "out", 
-                      },
-                  },
-                  size: {
-                      value: { min: 250, max: 300 },
-                  },
-                  opacity: {
-                      value: 1,
-                  },
-              },
-          },
+    id: "tsparticles",
+    options: {
+      fullScreen: {
+        enable: false,
       },
+      fpsLimit: 60,
+      particles: {
+        number: {
+          value: 0,
+        },
+        color: {
+          value: "#0B4599",
+        },
+        links: {
+          color: "#003399",
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          outModes: {
+            default: "bounce",
+          },
+        },
+        size: {
+          value: { min: 150, max: 200 },
+        },
+        opacity: {
+          value: 1,
+        },
+      },
+      emitters: {
+        direction: "none",
+        position: {
+          x: 50,
+          y: 50,
+        },
+        rate: {
+          quantity: particleQuantity,
+          delay: 1,
+        },
+        autoPlay: true,
+        life: {
+          duration: 1,
+          count: 1,
+        },
+        particles: {
+          move: {
+            speed: 1.5,
+            outModes: {
+              default: "out",
+            },
+          },
+          size: {
+            value: { min: 250, max: 300 },
+          },
+          opacity: {
+            value: 1,
+          },
+        },
+      },
+    },
   });
 })();
